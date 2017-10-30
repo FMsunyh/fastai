315966cbb07249cbaa594d026838be77fb23b2fd
fmsunyh <fmsunyh@gmail.com>
2017-10-30 17:43:09 +0800

    终于用上 GPU了，速度快很多

--- lesson4_statefarm-sample.py
+++ lesson4_statefarm-sample.py
@@ -1,6 +1,3 @@
-from theano.sandbox import cuda
-cuda.use('gpu1')
-
 from utils import *
 
 
@@ -12,31 +9,55 @@ batch_size=64
 batches = get_batches(path+'train', batch_size=batch_size)
 val_batches = get_batches(path+'valid', batch_size=batch_size)
 
-model = Sequential( [
-    BatchNormalization(axis=1,input_shape=(3,224,224)),
-    Convolution2D(32,3,3, activation='relu'),
-    BatchNormalization(axis=1),
-    MaxPooling2D((3,3)),
-    Convolution2D(64,3,3,activation='relu'),
-    BatchNormalization(axis=1),
-    MaxPooling2D((3,3)),
-    Flatten(),
-    Dense(200,activation='relu'),
-    BatchNormalization(),
-    Dense(10,activation='softmax')
-     ])
-
-
-
-# model.compile(Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+# model = Sequential( [
+#     BatchNormalization(axis=1,input_shape=(3,224,224)),
+#     Convolution2D(32,3,3, activation='relu'),
+#     BatchNormalization(axis=1),
+#     MaxPooling2D((3,3)),
+#     Convolution2D(64,3,3,activation='relu'),
+#     BatchNormalization(axis=1),
+#     MaxPooling2D((3,3)),
+#     Flatten(),
+#     Dense(200,activation='relu'),
+#     BatchNormalization(),
+#     Dense(10,activation='softmax')
+#      ])
+#
+#
+#
+# # model.compile(Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
+# # model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+#
+# # change the learning rate
+# model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 # model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+#
+#
+# model.optimizer.lr = 0.001
+# model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+# # model.summary()
 
-# change the learning rate
-model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
+def conv1(batches):
+    model = Sequential([
+            BatchNormalization(axis=1, input_shape=(3,224,224)),
+            Convolution2D(32,3,3, activation='relu'),
+            BatchNormalization(axis=1),
+            MaxPooling2D((3,3)),
+            Convolution2D(64,3,3, activation='relu'),
+            BatchNormalization(axis=1),
+            MaxPooling2D((3,3)),
+            Flatten(),
+            Dense(200, activation='relu'),
+            BatchNormalization(),
+            Dense(10, activation='softmax')
+        ])
 
+    model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                     nb_val_samples=val_batches.nb_sample)
+    model.optimizer.lr = 0.001
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches,
+                     nb_val_samples=val_batches.nb_sample)
+    return model
 
-model.optimizer.lr = 0.001
-model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches, nb_val_samples=val_batches.nb_sample)
-# model.summary()
-
+conv1(batches)
